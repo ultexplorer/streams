027588430511291cb6d7e8257f5e97174b97b15e
ultexplorer <ultexplorer@gmail.com>
2022-11-03 15:51:46 +0200

    all done

---
+++ 1-readable.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const fs = require('fs');
+
+const rs = fs.createReadStream('1-readable.js', 'utf8');
+
+rs.on('readable', () => {
+    console.log('readable');
+    const buffer = rs.read();
+    //console.dir({buffer})
+    if (buffer) {
+        console.log(buffer);
+        //console.log(buffer.toString());
+    }
+});
+
+rs.on('data', chunk => {
+    console.log('data');
+    console.log(chunk);
+})
